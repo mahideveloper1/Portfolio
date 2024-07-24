e14fccd21db0f11f139c943493fbeb3f0ca05d83
Mahesh Vashisth <maheshvashisth07@gmail.com>
2024-07-24 23:33:09 +0530

    made work component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Experience from "./components/Experience/Experience";
 import Footer from "./components/Footer/Footer";
+import Work from "./components/Work/Work";
 // import Testimonials from "./components/Testimonials/Testimonials";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <Nav />
       <About />
       <Experience />
+      <Work />
       <Portfolio />
       {/* <Testimonials /> */}
       <Contact />
---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Work.css";
+
+const workData = [
+  {
+    role: "React Developer",
+    company: "Harman International",
+    duration: "July 2023 - September 2023",
+    description:
+      "Worked on developing and maintaining web applications using React.js. Improved UI/UX designs to enhance user experience and collaborated closely with the backend team to integrate APIs seamlessly.",
+  },
+  {
+    role: "DevOps Engineer",
+    company: " As a Freelancer",
+    duration: "Jul 2024 - Present",
+    description:
+      "Assisted in setting up CI/CD pipelines using Gitlab, managed Docker containers, and deployed applications on AWS. Worked on infrastructure automation using Terraform.",
+  },
+];
+
+const Work = () => {
+  return (
+    <section id="work">
+      <h5>Where I've Worked</h5>
+      <h2>Work Experience</h2>
+      <div className="container work__container">
+        {workData.map((work, index) => (
+          <article key={index} className="work__item">
+            <h3>{work.role}</h3>
+            <h4>{work.company}</h4>
+            <p className="work__duration">{work.duration}</p>
+            <p>{work.description}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Work;
